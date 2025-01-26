f01a347ccb86ef5691d8bad720b84266e33dbc56
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-01-27 07:45:42 +0800

    First commit.

---
+++ copy-file.sh
@@ -0,0 +1,3 @@
+mv /c/Users/GRajeg/Downloads/Enggano\ Etymological\ List\.xlsx /c/Users/GRajeg/OneDrive\ -\ Nexus365/Documents/Research/enggano-etymological-list
+mkdir "data-raw"
+mv "Enggano Etymological List.xlsx" "data-raw/Enggano-Etymological-List.xlsx"
